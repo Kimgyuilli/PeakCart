c3e652d1d4bc8e5966efd457033b0048e03720bd
Kimgyuilli <rlarbdlf222@naver.com>
2026-03-27 11:35:20 +0900

    feat: OpenAPI 설정 추가 (프로젝트 메타정보 + JWT Bearer SecurityScheme)

---
+++ src/main/java/com/peekcart/global/config/OpenApiConfig.java
@@ -0,0 +1,31 @@
+package com.peekcart.global.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        String securitySchemeName = "Bearer Token";
+
+        return new OpenAPI()
+                .info(new Info()
+                        .title("PeekCart API")
+                        .description("대용량 트래픽 환경을 고려한 이커머스 플랫폼 API")
+                        .version("v1"))
+                .addSecurityItem(new SecurityRequirement().addList(securitySchemeName))
+                .components(new Components()
+                        .addSecuritySchemes(securitySchemeName, new SecurityScheme()
+                                .name(securitySchemeName)
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")));
+    }
+}
